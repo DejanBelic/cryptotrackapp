99fa43d787888c94cc97fd7da324b9e6c1b1ec94
Dejan <dejan.belic@develomon.com>
2018-08-18 15:10:59 +0200

    Implemented loading indicator on page load, while fetching data. Created presentational component loadingIndicator.

--- cra-cryptotrack/src/Container/Home/Home.js
+++ cra-cryptotrack/src/Container/Home/Home.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Home.css';
 import RowItem from '../../Presentational/rowItem/rowItem';
-
+import LoadingIndicator from '../../Presentational/loadingIndicator/loadingIndicator';
 
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [],
+            loading: false
         }
     }
 
     componentWillMount() {
+
+        // Set state to loading true which will trigger loading indicator.
+        this.setState({ loading: true });
+
         const apiURL = ' https://api.coinmarketcap.com/v2/ticker/';
         fetch(apiURL)
             .then((response) => { return response.json(); })
             .then(data => {
                 this.setState({
-                    data: data.data
+                    data: data.data,
+                    loading: false
                 })
                 console.log(data.data);
             })
@@ -26,23 +32,29 @@ class Home extends Component {
     }
 
     render() {
+        const { loading } = this.state;
+        const loader = (loading ? <LoadingIndicator /> : null);
+
+
         // Filter first 50 top ranked crpyto currencies.
         let topRankedValues = Object.values(this.state.data).filter(function (item) {
             return item.rank <= 50;
         });
+
         let tableRows = Object.values(topRankedValues).map(row => {
             return (
                 <RowItem
                     key={row.id}
                     name={row.name}
                     shortName={row.symbol}
-                    values= {parseFloat(row.quotes['USD'].price).toFixed(2)}
+                    values={parseFloat(row.quotes['USD'].price).toFixed(2)}
                     lastChanges={row.quotes['USD'].percent_change_24h}
                 />
             );
         });
         return (
             <React.Fragment>
+                {loader}
                 <div className="table-wrapper">
                     {tableRows}
                 </div>
---
+++ cra-cryptotrack/src/Presentational/loadingIndicator/loadingIndicator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  './loadingIndicator.css';
+
+const LoadingIndicator = () => (
+    <div id="wrapper">
+        <div className="spinner">
+            <div className="loading-container">
+                <div className="ball"></div>
+                <div className="ball-inner"></div>
+            </div>
+        </div>
+    </div>
+);
+
+export default LoadingIndicator;
